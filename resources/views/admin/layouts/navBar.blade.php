5ce5cb53d62990a53d446d2a78a25d80468fa4d6
RanaHanan11 <123240160+RanaHanan11@users.noreply.github.com>
2023-08-25 15:24:53 +0500

    done
    done

--- resources/views/admin/layouts/navBar.blade.php
+++ resources/views/admin/layouts/navBar.blade.php
@@ -80,7 +80,7 @@
                     <button href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
                         <img src="{{ asset('admin-dashboard/assets/img/user/user.png') }}"
                             class="user-image" alt="User Image" />
-                        <span class="d-none d-lg-inline-block">Abdus Salam</span>
+                        <span class="d-none d-lg-inline-block">{{ Auth::User()->name }}</span>
                     </button>
                     <ul class="dropdown-menu dropdown-menu-right">
                         <!-- User image -->
@@ -88,7 +88,7 @@
                             <img src="{{ asset('admin-dashboard/assets/img/user/user.png') }}"
                                 class="img-circle" alt="User Image" />
                             <div class="d-inline-block">
-                                Abdus Salam <small class="pt-1">abdus@gmail.com</small>
+                                {{ Auth::User()->name }} <small class="pt-1">{{ Auth::User()->email }}</small>
                             </div>
                         </li>
 
